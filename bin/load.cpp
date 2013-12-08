6d98ce1039ae606e3fee8309fb420832ecc76cc5
Anton Gladky <gladk@debian.org>
2013-12-08 14:30:53 +0100

    Initial commit.

---
+++ bin/load.cpp
@@ -0,0 +1,79 @@
+/*
+    This file is part of fensterkalk.
+    Copyright (C) 2013 Anton Gladky <gladky.anton@gmail.com>
+
+    Author: 2013, Anton Gladky <gladky.anton@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Gapart is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with fensterkalk.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
+
+#include <boost/foreach.hpp>
+#include <boost/lexical_cast.hpp>
+#include <Eigen/Core>
+
+#include <vector>
+
+using boost::property_tree::ptree;
+using boost::property_tree::read_json;
+using boost::property_tree::write_json;
+
+void writejson() {
+  ptree pt;
+    
+  std::vector <Eigen::Vector3d> nodes, nodes_read;
+  
+  nodes.push_back(Eigen::Vector3d(0,0,0));
+  nodes.push_back(Eigen::Vector3d(1110,0,0));
+  nodes.push_back(Eigen::Vector3d(555,2180,0));
+  
+  unsigned int i = 0;
+  pt.put("nodes_number", nodes.size());
+  
+  BOOST_FOREACH(const Eigen::Vector3d &node, nodes) {
+      pt.add("nodes.node_" + boost::lexical_cast<std::string>(i) + ".x", node(0));
+      pt.add("nodes.node_" + boost::lexical_cast<std::string>(i) + ".y", node(1));
+      pt.add("nodes.node_" + boost::lexical_cast<std::string>(i) + ".z", node(2));
+      i++;
+  }
+  
+  write_json ("example.json", pt);
+  
+  ptree pt_read;
+
+  read_json("example.json", pt_read);
+  
+  
+  int nodes_number = pt.get<int>("nodes_number", -1);
+  
+  if (nodes_number>0) {
+    for (unsigned i=0; i < nodes_number; i++){
+      nodes_read.push_back(Eigen::Vector3d(
+        pt.get<double>("nodes.node_"+boost::lexical_cast<std::string>(i)+".x"), 
+        pt.get<double>("nodes.node_"+boost::lexical_cast<std::string>(i)+".y"), 
+        pt.get<double>("nodes.node_"+boost::lexical_cast<std::string>(i)+".z")));
+    }
+  }
+  
+  
+  BOOST_FOREACH(Eigen::Vector3d i, nodes_read) {
+    std::cerr<<i(0)<< " " << i(1) << " " << i(2) <<std::endl;
+  }
+}
+
+int main(int ac, char* av[]) { 
+  writejson();
+}
---
+++ bin/main.cpp
@@ -0,0 +1,75 @@
+/*
+    This file is part of fensterkalk.
+    Copyright (C) 2013 Anton Gladky <gladky.anton@gmail.com>
+
+    Author: 2013, Anton Gladky <gladky.anton@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Gapart is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with fensterkalk.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "main.h"
+#include "log.h"
+
+namespace po = boost::program_options;
+namespace fs = boost::filesystem;
+
+
+int main(int ac, char* av[])
+{ 
+  boost::shared_ptr<logs> log (new logs());
+  
+  std::cout<<"\n\
+Fensterkalk \n\
+Copyright (C) 2013 Anton Gladky <gladky.anton@gmail.com> \n\
+This program comes with ABSOLUTELY NO WARRANTY.\n\
+"<<std::endl;
+  
+  try {
+    po::options_description desc("Allowed options");
+    desc.add_options()
+      ("help", "produce help message")
+      ("input,i", po::value<std::string>(), "input file name")
+    ;
+    
+    po::positional_options_description p;
+    p.add("input", -1);
+    po::variables_map vm;        
+    po::store(po::command_line_parser(ac, av).
+    options(desc).positional(p).run(), vm);
+    po::notify(vm);  
+    
+    if (vm.count("help")) {
+      std::cout << desc << std::endl;
+      return 0;
+    }
+    
+    if (vm.count("input")) {
+      std::string ss = "input file is: " + vm["config"].as<std::string>();
+      log->info(ss);
+    } else {
+      std::string ss = "input file is required, use `-i` option for that or `--help`.\n";
+      log->info(ss);
+      exit (EXIT_FAILURE);
+    }
+    //configFileName = vm["config"].as<string>();
+  }
+   catch(std::exception& e) {
+      std::string ss =  "error: " + std::string(e.what());
+      log->info(ss);
+      exit (EXIT_FAILURE);
+  }
+  catch(...) {
+      std::cerr << "Exception of unknown type!\n";
+  }
+}
