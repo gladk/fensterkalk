cf8f2ff8ce9fe8318e4d916b50f3f6dfdcd3ae1a
Anton Gladky <gladk@debian.org>
2018-02-16 22:06:54 +0100

    Add one test

--- lib/test.cpp
+++ lib/test.cpp
@@ -18,10 +18,19 @@
     along with fensterkalk.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include "constr.h"
 #include <gtest/gtest.h>
 
-TEST(test01, allTest) {
-  for (unsigned short i = 1; i <= 4; i++) {
-    EXPECT_EQ(1, 2 - 1);
-  }
+TEST(PrimitiveTest, CreateConstruction) {
+  using namespace Eigen;
+  std::shared_ptr<Constr> constrTMP = std::make_shared<Constr>(Constr());
+  // The empty construction should be not calculated
+  EXPECT_EQ(false, constrTMP->calculated());
+  // Add nodes into the construction
+  EXPECT_EQ(true, constrTMP->addNodeMainFrame(std::move(Vector3d(0, 0, 0))));
+  EXPECT_EQ(true, constrTMP->addNodeMainFrame(std::move(Vector3d(2, 0, 0))));
+  EXPECT_EQ(true, constrTMP->addNodeMainFrame(std::move(Vector3d(2, 2, 0))));
+  EXPECT_EQ(true, constrTMP->checkFrames());
+  // Three nodes should be available
+  EXPECT_EQ(3, (constrTMP->mainFrameNodes()).size());
 }
